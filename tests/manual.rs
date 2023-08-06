3933fd876c0e931ce774242a3b9ee6defadad412
Mustafa S. Moiz <msmoiz@amazon.com>
2023-08-06 00:10:07 -0700

    test: round out manual tests

--- tests/manual.rs
+++ tests/manual.rs
@@ -1,20 +1,10 @@
-use indoc::indoc;
+mod macros;
+
 use std::collections::HashMap;
 
-use stache::{Context, Result, Template};
-
-#[test]
-fn intro() -> Result<()> {
-    let text = indoc! {"
-        Hello {{name}}
-        You have just won {{value}} dollars!
-        {{#in_ca}}
-        Well, {{taxed_value}} dollars, after taxes.
-        {{/in_ca}}
-    "};
-
-    let template = Template::compile(text)?;
-    let context = Context::Map(HashMap::from([
+mstest!(
+    intro,
+    Context::Map(HashMap::from([
         ("name".into(), Context::String("Mustafa".into())),
         ("value".into(), Context::Integer(10000)),
         (
@@ -22,88 +12,192 @@ fn intro() -> Result<()> {
             Context::Float(10000.0 - (10000.0 * 0.4)),
         ),
         ("in_ca".into(), Context::Bool(true)),
-    ]));
-    let rendered = template.render(context);
+    ])),
+    "
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Hello {{name}}
+    You have just won {{value}} dollars!
+    {{#in_ca}}
+    Well, {{taxed_value}} dollars, after taxes.
+    {{/in_ca}}
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Hello Mustafa
+    You have just won 10000 dollars!
+    Well, 6000 dollars, after taxes.
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    "
+);
 
-    let expected = indoc! {"
-        Hello Mustafa
-        You have just won 10000 dollars!
-        Well, 6000 dollars, after taxes.
-    "};
-
-    assert_eq!(rendered, expected);
-
-    Ok(())
-}
-
-#[test]
-fn variables() -> Result<()> {
-    let text = indoc! {"
-        * {{name}}
-        * {{age}}
-        * {{company}}
-        * {{{company}}}
-    "};
-
-    let template = Template::compile(text)?;
-    let context = Context::Map(HashMap::from([
+mstest!(
+    variables,
+    Context::Map(HashMap::from([
         ("name".into(), Context::String("Mustafa".into())),
         ("company".into(), Context::String("<b>GitHub</b>".into())),
-    ]));
-    let rendered = template.render(context);
+    ])),
+    "
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    * {{name}}
+    * {{age}}
+    * {{company}}
+    * {{{company}}}
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    * Mustafa
+    * 
+    * &lt;b&gt;GitHub&lt;/b&gt;
+    * <b>GitHub</b>
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    "
+);
 
-    let expected = indoc! {"
-        * Mustafa
-        * 
-        * &lt;b&gt;GitHub&lt;/b&gt;
-        * <b>GitHub</b>
-    "};
+mstest!(
+    section_false,
+    Context::Map(HashMap::from([("person".into(), Context::Bool(false))])),
+    "
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Shown.
+    {{#person}}
+      Never shown!
+    {{/person}}
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Shown.
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    "
+);
 
-    assert_eq!(rendered, expected);
+mstest!(
+    section_non_empty_list,
+    Context::Map(HashMap::from([(
+        "repo".into(),
+        Context::List(vec![
+            Context::Map(HashMap::from([(
+                String::from("name"),
+                Context::String("resque".into())
+            ),])),
+            Context::Map(HashMap::from([(
+                String::from("name"),
+                Context::String("hub".into())
+            )])),
+            Context::Map(HashMap::from([(
+                String::from("name"),
+                Context::String("rip".into())
+            ),]))
+        ])
+    )])),
+    "
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    {{#repo}}
+      <b>{{name}}</b>
+    {{/repo}}
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      <b>resque</b>
+      <b>hub</b>
+      <b>rip</b>
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    "
+);
 
-    Ok(())
-}
+mstest!(
+    non_false_values,
+    Context::Map(HashMap::from([(
+        String::from("person?"),
+        Context::Map(HashMap::from([(
+            String::from("name"),
+            Context::String("Jon".into())
+        )]))
+    )])),
+    "
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    {{#person?}}
+      Hi {{name}}!
+    {{/person?}}
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      Hi Jon!
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    "
+);
 
-#[test]
-fn section_false() -> Result<()> {
-    let text = indoc! {"
-        Shown.
-        {{#person}}
-        Never shown!
-        {{/person}}
-    "};
+mstest!(
+    inverted_sections,
+    Context::Map(HashMap::from([(
+        String::from("repo"),
+        Context::List(vec![])
+    )])),
+    "
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    {{#repo}}
+      <b>{{name}}</b>
+    {{/repo}}
+    {{^repo}}
+      No repos :(
+    {{/repo}}
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      No repos :(
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    "
+);
 
-    let template = Template::compile(text)?;
-    let context = Context::Map(HashMap::from([("person".into(), Context::Bool(false))]));
-    let rendered = template.render(context);
+mstest!(
+    comments,
+    Context::Map(HashMap::from([])),
+    "
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    <h1>Today{{! ignore me }}.</h1>
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    <h1>Today.</h1>
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    "
+);
 
-    let expected = indoc! {"
-        Shown.
-    "};
+mstest_with_partials!(
+    partials,
+    Context::Map(HashMap::from([(
+        String::from("names"),
+        Context::List(vec![
+            Context::Map(HashMap::from([(
+                String::from("name"),
+                Context::String("Bob".into())
+            )])),
+            Context::Map(HashMap::from([(
+                String::from("name"),
+                Context::String("Billy".into())
+            )]))
+        ])
+    )])),
+    HashMap::from([(
+        String::from("user"),
+        String::from("<strong>{{name}}</strong>\n")
+    )]),
+    "
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    <h2>Names</h2>
+    {{#names}}
+      {{> user}}
+    {{/names}}
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    <h2>Names</h2>
+      <strong>Bob</strong>
+      <strong>Billy</strong>
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    "
+);
 
-    assert_eq!(rendered, expected);
-
-    Ok(())
-}
-
-#[test]
-fn section_empty_list() -> Result<()> {
-    let text = indoc! {"
-        Shown.
-        {{#person}}
-        Never shown!
-        {{/person}}
-    "};
-
-    let template = Template::compile(text)?;
-    let context = Context::Map(HashMap::from([("person".into(), Context::List(vec![]))]));
-    let rendered = template.render(context);
-
-    let expected = indoc! {"
-        Shown.
-    "};
-
-    assert_eq!(rendered, expected);
-
-    Ok(())
-}
+mstest!(
+    set_delimiter,
+    Context::Map(HashMap::from([(
+        String::from("greeting"),
+        Context::String("hello".into())
+    )])),
+    "
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    * {{greeting}}
+    {{=<% %>=}}
+    * <% greeting %>
+    <%={{ }}=%>
+    * {{ greeting }}
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    * hello
+    * hello
+    * hello
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    "
+);
